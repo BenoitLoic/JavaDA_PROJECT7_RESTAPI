37751a93e9685fb30a5027b8e52c1e726a96b370
loic <shin.loicb@gmail.om>
2021-08-08 10:14:25 +0200

    Added javadoc for UserController

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -14,23 +14,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
+/**
+ * Contains CRUD method for User model/entity.
+ */
 @Controller
 public class UserController {
+
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * This method will get all user in data base.
+     *
+     * @param model the model that serve to inject user list for the view
+     * @return the list of all user.
+     */
     @RequestMapping("/user/list")
-    public String home(Model model)
-    {
+    public String home(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
 
+    /**
+     * This method will return the form to add a new user.
+     *
+     * @return the html form.
+     */
     @GetMapping("/user/add")
-    public String addUser(User bid) {
+    public String addUserForm(User user) {
         return "user/add";
     }
 
+    /**
+     * This method will create a new user.
+     *
+     * @param user   the user to create
+     * @param result validation check for user.
+     * @param model  the model that serve to inject user list for the view
+     * @return redirection to /user/list (get all user)
+     */
     @PostMapping("/user/validate")
     public String validate(@Valid User user, BindingResult result, Model model) {
         if (!result.hasErrors()) {
@@ -43,6 +65,13 @@ public class UserController {
         return "user/add";
     }
 
+    /**
+     * This method will return the update form with user's data with the given id.
+     *
+     * @param id    the id of the user to get data.
+     * @param model the model where we inject the user data.
+     * @return the form with user's data inside.
+     */
     @GetMapping("/user/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
@@ -51,6 +80,15 @@ public class UserController {
         return "user/update";
     }
 
+    /**
+     * This method will update the user with the given id.
+     *
+     * @param id     the id of the user to update
+     * @param user   the user to update
+     * @param result validation check
+     * @param model  the model that serve to inject user list for the view
+     * @return redirection to /user/list (get all user)
+     */
     @PostMapping("/user/update/{id}")
     public String updateUser(@PathVariable("id") Integer id, @Valid User user,
                              BindingResult result, Model model) {
@@ -66,6 +104,13 @@ public class UserController {
         return "redirect:/user/list";
     }
 
+    /**
+     * This method delete the user with the given id.
+     *
+     * @param id    the id of the user to delete
+     * @param model the model that serve to inject user list for the view
+     * @return redirection to /user/list (get all user)
+     */
     @GetMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/dto/CreateUser.java
@@ -0,0 +1,92 @@
+package com.nnk.springboot.dto;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+public class CreateUser {
+
+
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+
+    @NotBlank(message = "Fullname is mandatory")
+    private String fullname;
+
+
+    @NotBlank(message = "Role is mandatory")
+    private String role;
+
+    public CreateUser() {
+    }
+
+    public CreateUser(String username, String password, String fullname, String role) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateUser{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", fullname='" + fullname + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CreateUser that = (CreateUser) o;
+        return Objects.equals(username, that.username) && Objects.equals(password, that.password) && Objects.equals(fullname, that.fullname) && Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, fullname, role);
+    }
+}
